72ad6a9768dd47389bbe789178309f0f1c2b3393
Bogdan Tirca <bogdantirca92@gmail.com>
2015-05-05 11:51:27 +0300

    Fix: Identare gresita 2

--- descompune/bogdan.cpp
+++ descompune/bogdan.cpp
@@ -24,42 +24,42 @@ int minCost;
 vector <string> words;
 
 void read() {
-	string cuv;
-	int cost;
-	ifstream fin("descompune.in");
-	fin >> prop;
-	fin >> n;
-	for (int i = 0; i < n; ++i) {
-		fin >> cuv >> cost;
-		dict[cuv] = cost;
-	}
+  string cuv;
+  int cost;
+  ifstream fin("descompune.in");
+  fin >> prop;
+  fin >> n;
+  for (int i = 0; i < n; ++i) {
+    fin >> cuv >> cost;
+    dict[cuv] = cost;
+  }
 }
 
 void solve() {
-	string word;
-	map<string,int>::iterator it;
-	for (int right = 0; right < prop.length(); ++right) {
-		dp[right + 1] = INF;
-		for (int left = max(0, right - WORD_LEN); left <= right; ++left) {
-			word = prop.substr(left, right - left + 1);
-			it = dict.find(word);
-			if (it != dict.end() && dp[left] + it->second < dp[right + 1]) {
-				dp[right + 1] = dp[left] + it->second;
-				back[right + 1] = left;
-			}
-		}
-		word.clear();
-	}
-	// Reconstituie solutia de cost minim
-	minCost = dp[prop.length()];
-	if (minCost != INF) {
-		int left, right = prop.length();
-		while ((left = back[right]) != 0) {
-			words.push_back(prop.substr(left, right - left));
-			right = left;
-		}
-		words.push_back(prop.substr(0, right));
-	}
+  string word;
+  map<string,int>::iterator it;
+  for (int right = 0; right < prop.length(); ++right) {
+    dp[right + 1] = INF;
+    for (int left = max(0, right - WORD_LEN); left <= right; ++left) {
+      word = prop.substr(left, right - left + 1);
+      it = dict.find(word);
+      if (it != dict.end() && dp[left] + it->second < dp[right + 1]) {
+        dp[right + 1] = dp[left] + it->second;
+        back[right + 1] = left;
+      }
+    }
+    word.clear();
+  }
+  // Reconstituie solutia de cost minim
+  minCost = dp[prop.length()];
+  if (minCost != INF) {
+    int left, right = prop.length();
+    while ((left = back[right]) != 0) {
+      words.push_back(prop.substr(left, right - left));
+      right = left;
+    }
+    words.push_back(prop.substr(0, right));
+  }
 }
 
 void write() {
@@ -75,7 +75,7 @@ void write() {
 }
 
 int main() {
-	read();
-	solve();
-	write();
+  read();
+  solve();
+  write();
 }
