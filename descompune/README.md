e742379bf1b7cbc9d904ad2d2966106af237c1e7
Bogdan Tirca <bogdantirca92@gmail.com>
2015-05-11 14:45:17 +0300

    Add brut

--- descompune/README.md
+++ descompune/README.md
@@ -1,14 +1,14 @@
 #Enunt
 
-Se da un dictionar de cuvinte si un sir de caractere ce reprezinta o propozitie din care s-au eliminat spatiile. Fiecare cuvant din dictionar are asociat un cost. Sa se afiseze o descompunere valida a propozitiei precum si costul minim al unei descompuneri.
+Se da un dictionar de cuvinte si un sir de caractere ce reprezinta o propozitie din care s-au eliminat spatiile. Fiecare cuvant din dictionar are asociat un cost. Sa se afiseze o descompunere valida de cost minim a propozitiei precum si costul minim al acelei descompuneri.
 
 #Date de intrare
 
 Pe prima linia se va afla un sir de caractere fara spatii ce reprezinta propozitia care va trebui descompusa. Pe linia a 2-a va fi un numar N, ce reprezinta numarul de cuvinte din dictionar urmat de N linii astfel:  linia i + 2 contine un cuvant CUV(i) fara spatii urmat de un numar COST(i) ce reprezinta costul asociat cuvantului i.
 
 #Date de iesire
-Pe prima linie se va afisa o descompunere valida a propozitiei folosind cuvintele din dictionar.
-Pe a 2-a linie se va afisa costul minim al unei descompuneri.
+Pe prima linie se va afisa o descompunere valida de cost minim a propozitiei folosind cuvintele din dictionar.
+Pe a 2-a linie se va afisa costul minim al acelei descompuneri.
 In cazul in care nu exista nicio descompunere valida, se va afisa -1.
 
 
--- descompune/bogdan_30.cpp
+++ descompune/bogdan_30.cpp
@@ -14,13 +14,19 @@ using namespace std;
 #define PROP_LEN 1001
 #define WORD_LEN 50
 
+// Optimisations
+#define OPT1 true
+
 string prop;
 int n;
 vector <pair <string, int> > dict;
 int dp[PROP_LEN], back[PROP_LEN];
+int backSol[PROP_LEN];
 
 int minCost;
 vector <string> words;
+
+// ofstream fout("descompune.out");
 
 void read() {
   string cuv;
@@ -52,26 +58,40 @@ bool matchEnd(string &word, int right) {
   return (r2 == -1);
 }
 
+void backt(int propPos) {
+	if (propPos == prop.length() - 1) {
+		// Solutie
+		if (minCost > dp[prop.length()]) {
+			minCost = dp[prop.length()];
+			memcpy(backSol, back, sizeof(int) * PROP_LEN);
+		}
+	} else {
+	  for (auto &word : dict) {
+	  	int right = propPos + word.first.length();
+	  	int left = propPos + 1;
+	  	if (right < prop.length() && matchEnd(word.first, right)) {
+	  		if (OPT1 && dp[left] + word.second < dp[right + 1] || !OPT1) {
+		  		dp[right + 1] = dp[left] + word.second;
+		  		back[right + 1] = left;
+		  		backt(right);
+		  	}
+	  	}
+	  }
+	}
+}
+
 void solve() {
-  string word;
-  for (int right = 0; right < prop.length(); ++right) {
-    dp[right + 1] = INF;
-    for (auto &word : dict) {
-      // Verifica daca 'word' se potriveste la capatul propozitiei
-      int left = right - word.first.length() + 1;
-      if (matchEnd(word.first, right) && dp[left] + word.second < dp[right + 1]) {
-        dp[right + 1] = dp[left] + word.second;
-        back[right + 1] = left;
-      }
-    }
-    word.clear();
+	minCost = INF;
+	for (int right = 1; right <= prop.length(); ++right) {
+    dp[right] = INF;
   }
+  
+  backt(-1);
 
   // Reconstituim solutia de cost minim
-  minCost = dp[prop.length()];
   if (minCost != INF) {
     int left, right = prop.length();
-    while ((left = back[right]) != 0) {
+    while ((left = backSol[right]) != 0) {
       words.push_back(prop.substr(left, right - left));
       right = left;
     }
