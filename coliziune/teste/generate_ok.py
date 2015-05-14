85748ff761ef1373a9828829a447c7b83db246de
Alex Palcuie <alex.palcuie@hootsuite.com>
2015-05-14 23:36:54 +0300

    Print time it took to run the source

--- coliziune/teste/generate_ok.py
+++ coliziune/teste/generate_ok.py
@@ -1,8 +1,9 @@
+import subprocess
 from sh import cp, rm
 import sh
 import os
 
-SURSA_CPP = 'main.cpp'
+SURSA_CPP = 'medie.cpp'
 PROBLEMA = 'coliziune'
 
 cp('../' + SURSA_CPP, '.')
@@ -11,7 +12,7 @@ os.system('g++ ' + SURSA_CPP)
 for i in range(1, 11):
   print 'Testul ', i
   cp(PROBLEMA + str(i) + '.in', PROBLEMA + '.in')
-  os.system('./a.out')
+  print subprocess.check_output('time ./a.out', shell=True)
   cp(PROBLEMA + '.out', PROBLEMA + str(i) + '.ok')
 
 for extension in ['in', 'out']:
