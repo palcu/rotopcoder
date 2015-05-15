3e1b5ff87d249aae4abebfe2b80966be321f530f
Dragos Alin <r.dragos0@gmail.com>
2015-05-15 12:08:45 +0300

    added cool verifier

--- coliziune/verif.cpp
+++ coliziune/verif.cpp
@@ -2,9 +2,9 @@
 #define ll long long
 using namespace std;
 
-void give_score(int score) {
-  cerr << "score: " << score << "\n";
-  exit(0);
+void printResult(int score, string message, int exit_status = 0) {
+  cout << score << " " << message;
+  exit(exit_status);
 }
 ll get_hash(string A, ll M, ll B) {
   ll val = 0;
@@ -21,34 +21,41 @@ ll get_hash(string A, ll M, ll B) {
   return val;
 }
 int main() {
-#ifndef ONLINE_JUDGE
   ifstream prb_in("coliziune.in");
-  ifstream prb_out("coliziune.out");
-#endif
 
+  if (!prb_in.is_open()) {
+    printResult(0, "Fisier de intrare lipsa", -1);
+  }
 
   ll M, B;
-  assert(prb_in >> M >> B);
+  if (!(prb_in >> M >> B)) {
+    printResult(0, "Fisier de intrare incomplet", -1);
+  }
   assert(2 <= B && B <= M - 2);
   assert(4 <= M && M <= 1e14);
 
   prb_in.close();
 
   string X, Y;
+
+  ifstream prb_out("coliziune.out");
+  if (!prb_out.is_open()) {
+    printResult(0, "Fisier de iesire incomplet", -1);
+  }
   if (!(prb_out >> X >> Y)) {
-    give_score(0);
+    printResult(0, "Fisier de iesire incomplet");
   }
   string Z; prb_out >> Z;
   if (Z != "") {
-    give_score(0);
+    printResult(0, "Prea multe siruri in fisierul de iesire");
   }
   if (X.size() > 1e6 || Y.size() > 1e6) {
-    give_score(0);
+    printResult(0, "Sirurile depasesc lungimea maxima admisa");
   }
   if (get_hash(X, M, B) == get_hash(Y, M, B)) {
-    give_score(10);
+    printResult(10, "Ai nimerit!");
   } else {
-    give_score(0);
+    printResult(0, "Functia H are valori diferite");
   }
   return 0;
 }
